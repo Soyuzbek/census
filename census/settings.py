7dc37ded1c22f1098920c8e1436dbb8422a5a230
unknown <Nurlan@Admin-PC.stat.kg>
2019-12-25 09:06:26 +0600

    User Creation Form form was changed

--- census/settings.py
+++ census/settings.py
@@ -97,14 +97,22 @@ WSGI_APPLICATION = 'census.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'censusdb',
+#         'USER': 'soyuz',
+#         'PASSWORD': 'census',
+#         'HOST': 'localhost',
+#         'PORT': ''
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'censusdb',
-        'USER': 'soyuz',
-        'PASSWORD': 'census',
-        'HOST': 'localhost',
-        'PORT': ''
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
