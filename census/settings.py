6d1ac3a9d7eda971f44064bde84c406015fde570
NurlanEmilbekov <nurlan.emilbekov.kg@gmail.com>
2020-01-14 16:00:35 +0600

    MMM

--- census/settings.py
+++ census/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#&3pc3!^34g6rfs*8+gb2*&5j95^#z5^*q6m+8_!-4+f1znrz^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
