5108b7b8e049ae38fe783fdf797eb73e125525d7
NurlanEmilbekov <nurlan.emilbekov.kg@gmail.com>
2020-01-20 12:46:22 +0600

    settings.py file has been changed

--- census/settings.py
+++ census/settings.py
@@ -33,7 +33,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#&3pc3!^34g6rfs*8+gb2*&5j95^#z5^*q6m+8_!-4+f1znrz^'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
