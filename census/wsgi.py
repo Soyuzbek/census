38da4c453132aef73bc8020c8a620bdb200c7244
NurlanEmilbekov <nurlan.emilbekov.kg@gmail.com>
2020-01-20 18:23:41 +0600

    redo

--- census/wsgi.py
+++ census/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'census.settings.prod')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'census.settings')
 
 application = get_wsgi_application()
