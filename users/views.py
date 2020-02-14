d7e77c936ceacd9fda6a9a5ceeec8cf8e40012f8
NurlanEmilbekov <nurlan.emilbekov.kg@gmail.com>
2020-02-14 09:30:26 +0600

    face cropper

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from io import BytesIO
 
+import piexif
 from PIL import Image
 from django.conf import settings
 from django.contrib import messages
@@ -199,10 +200,38 @@ class PhotoUpdateView(UpdateView):
         y2 = int(form.cleaned_data['y2'])
         image = form.cleaned_data['photo']
         img = Image.open(image)
-        profile_img = img.crop((x1, y1, x2, y2))
+        img1 = rotate_jpeg(img)
+        profile_img = img1.crop((x1, y1, x2, y2))
         profile_img.save(img_io, format='JPEG', quality=100)
         profile_img.seek(0)
         image = ContentFile(img_io.getvalue(), 'profile.jpg')
         self.object.photo = image
         self.object.save()
         return redirect('users:update', pk=self.object.pk)
+
+
+def rotate_jpeg(img):
+    if "exif" in img.info:
+        exif_dict = piexif.load(img.info["exif"])
+
+        if piexif.ImageIFD.Orientation in exif_dict["0th"]:
+            orientation = exif_dict["0th"].pop(piexif.ImageIFD.Orientation)
+            exif_bytes = piexif.dump(exif_dict)
+
+            if orientation == 2:
+                img = img.transpose(Image.FLIP_LEFT_RIGHT)
+            elif orientation == 3:
+                img = img.rotate(180)
+            elif orientation == 4:
+                img = img.rotate(180).transpose(Image.FLIP_LEFT_RIGHT)
+            elif orientation == 5:
+                img = img.rotate(-90, expand=True).transpose(Image.FLIP_LEFT_RIGHT)
+            elif orientation == 6:
+                img = img.rotate(-90, expand=True)
+            elif orientation == 7:
+                img = img.rotate(90, expand=True).transpose(Image.FLIP_LEFT_RIGHT)
+            elif orientation == 8:
+                img = img.rotate(90, expand=True)
+
+            img.save('profile.jpeg', exif=exif_bytes)
+    return img
