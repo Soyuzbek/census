90b133b8102a897cb42fe880c9a07d2e98c205e2
soyuzbek <soyuzbek196.kg@gmail.com>
2020-03-11 20:53:45 +0600

    dismissed filter added

--- users/filters.py
+++ users/filters.py
@@ -1,9 +1,17 @@
 import django_filters
 
-from users.models import Employee
+from users.models import Employee, Territory
+
+
+def territories(request):
+    if request is None:
+        return Territory.objects.all()
+    return Territory.objects.filter(district=request.user.district)
 
 
 class EmployeeFilter(django_filters.FilterSet):
+    territory = django_filters.ModelChoiceFilter(queryset=territories)
+
     class Meta:
         model = Employee
         fields = {
@@ -11,6 +19,7 @@ class EmployeeFilter(django_filters.FilterSet):
             'first_name': ['icontains'],
             'last_name': ['icontains'],
             'role': ['exact'],
-            'is_badge_printed': ['exact']
+            'dismissed': ['exact'],
+            'is_badge_printed': ['exact'],
 
         }
